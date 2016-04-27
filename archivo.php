5c2c092a59f9575e6b5e04ac4e04023847cd899a
#manosenelcódigo <peligrosamente@gmail.com>
2016-04-27 10:22:54 -0300

    archivo.php

---
+++ archivo.php
@@ -0,0 +1,15 @@
+<?php
+//declaración de la variable
+//asignación de la variable
+//dos reservas en tiempo de ejecución del script
+
+$num1 = 13;
+
+//echo $num1;
+
+$num2='lo que sea ñandú';
+
+//reasignación de una variable
+$num1= $num1 + 45;
+echo $num1;
+
