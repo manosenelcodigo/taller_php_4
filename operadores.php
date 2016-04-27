7d1447ac1cdebc5b0c8cc59931b6d5ac068e24fc
#manosenelcódigo <peligrosamente@gmail.com>
2016-04-27 10:23:32 -0300

    operadores.php

---
+++ operadores.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Operadores de asignación
+    
+    = asignación
+*/
+/*
+Operadores de comparación
+    
+    > mayor qué
+    < menor qué
+    <= menor o igual
+    >= mayor o igual
+    == igual
+    === igual exacto
+    != distinto de
+ 
+*/
+/*
+    Operadores aritméticos
+    
+    + suma
+    - resta
+    * multiplicación
+    / división
+    % resto de una división
+*/
+/*
+    Operadores Lógicos
+    
+        and &&
+        or  || 
+        not
+        ! negar una condición
+*/
+
+
